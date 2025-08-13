581df609515daf7cc2cfe08ca50b4422ecebb080
sharku <alirashini2323@outlook.com>
2025-08-13 20:17:07 +0200

    refactored authentication

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom';
+
 import Home from './views/Home';
 import Login from './views/Login';
+
 import Callback from './views/criipto/Callback';
+
+import Dashboard from './views/velia/Dashboard';
 
 export default function App() {
   return (
@@ -11,6 +15,8 @@ export default function App() {
       <Route path='/Login' element={<Login />} />
 
       <Route path='/criipto/callback' element={<Callback />} />
+
+      <Route path='/dashboard' element={<Dashboard />} />
     </Routes>
   )
 }
---
+++ src/controllers/AuthenticationController.js
@@ -0,0 +1,47 @@
+import React, {useEffect, useState} from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+import axios from 'axios';
+
+export default function AuthenticationController() {
+    const navigate = useNavigate();
+    const { state } = useLocation();
+    const { setUser } = useAuth();
+    const [error, setError] = useState(null);
+    
+    useEffect(() => {
+        const run = async() => {
+            try
+            {
+                const idToken = state?.idToken || sessionStorage.getItem('id_token');
+                axios.post('/api/v1/auth/bankid/login/', { id_token: idToken })
+                .then(response => {
+                    const user = response.data;
+                    setUser(user);
+                    sessionStorage.setItem('user', JSON.stringify(user));
+                    sessionStorage.removeItem('id_token');
+                    sessionStorage.removeItem('claims');
+                    
+                    navigate('/dashboard', { replace: true });
+                })
+                .catch(error => {
+                    console.error("Authentication error:", error);
+                    setError("Authentication failed. Please try again.");
+                    navigate('/Login', { replace: true });
+                });
+            }
+            catch(e)
+            {
+                console.error("Error during authentication:", e);
+                setError("An unexpected error occurred. Please try again.");
+                navigate('/Login', { replace: true });
+            }
+        }
+    }, []);
+
+    return (
+        <div className="w-full h-screen flex items-center justify-center">
+            <p className="text-[32px] bg-blue-400">Authenticating...</p>
+        </div>
+    )
+}
