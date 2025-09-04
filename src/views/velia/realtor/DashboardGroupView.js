f15a248e7d3764dd515f175a8d3ab4b07504534b
sharku <alirashini2323@outlook.com>
2025-09-04 11:32:40 +0200

    udpate

--- src/views/velia/realtor/DashboardGroupView.js
+++ src/views/velia/realtor/DashboardGroupView.js
@@ -371,6 +371,8 @@ export default function DashboardGroupView() {
         })
     }
 
+    console.log(group);
+
     return (
         <div className="flex flex-col w-full h-full">
             {renderPopup()}
